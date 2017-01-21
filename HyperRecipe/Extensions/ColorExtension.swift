a2fcdcfadec2b09b9a74625f2d30624f172dd3bb
Rebouh Aymen <aymenworks@gmail.com>
2017-01-21 17:23:50 +0100

    Add the app main colors in an extension

---
+++ HyperRecipe/Extensions/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtension.swift
+//  HyperRecipe
+//
+//  Created by Rebouh Aymen on 21/01/2017.
+//  Copyright © 2017 Aymen Rebouh. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  static let hyperDarkBlue = #colorLiteral(red: 0.2469966114, green: 0.3779316545, blue: 0.4080758095, alpha: 1)
+  static let hyperBeige    = #colorLiteral(red: 0.9310407043, green: 0.9225626588, blue: 0.8535305858, alpha: 1)
+  static let hyperOrange = #colorLiteral(red: 0.9698477387, green: 0.5682272315, blue: 0.2841275036, alpha: 1)
+  static let hyperBrown = #colorLiteral(red: 0.8176086545, green: 0.6650074124, blue: 0.5771699548, alpha: 1)
+}
