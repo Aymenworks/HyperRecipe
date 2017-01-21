12f65f47b57ca237312127ff6a6d104e03eb91a7
Rebouh Aymen <aymenworks@gmail.com>
2017-01-21 12:24:00 +0100

    Inherit from TabBarController for adding and handling a add recipe action
    I customize as well the tab bar tint color

--- HyperRecipe/ViewControllers/BaseViewController.swift
+++ HyperRecipe/ViewControllers/BaseViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BaseViewController: UIViewController {
   
+  // MARK: - Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
---
+++ HyperRecipe/ViewControllers/RecipeTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeTabBarController.swift
+//  HyperRecipe
+//
+//  Created by Rebouh Aymen on 21/01/2017.
+//  Copyright © 2017 Aymen Rebouh. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTabBarController: UITabBarController {
+  
+  // MARK: - Properties
+  
+  var addRecipeButton: UIButton!
+  
+  // MARK: - Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    addRecipeButton = UIButton(type: .custom)
+    addRecipeButton.setImage(#imageLiteral(resourceName: "addRecipe"), for: .normal)
+    addRecipeButton.addTarget(self, action: #selector(showRecipeFormController), for: .touchUpInside)
+    
+    view.addSubview(addRecipeButton)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    addRecipeButton.frame = CGRect(x: tabBar.frame.width/2 - 20, y: tabBar.frame.minY - 20, width: 40, height: 40)
+  }
+  
+  // MARK: - User interaction
+  
+  func showRecipeFormController() {
+    print("show recipe form controller")
+  }
+  
+}
